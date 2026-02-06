0fbd294c19d888adec317ad2c8c3dc94381ddd5c
Ben Minter <benminter97@gmail.com>
2026-02-06 19:06:56 +0000

    just make it work for now

--- public/_bootstrap.php
+++ public/_bootstrap.php
@@ -1,3 +1,4 @@
 <?php
 // _bootstrap.php
-define('SIMPLESAMLPHP_UPSTREAM', getenv('HOME') . '/repositories/simplesamlphp');
+define('SIMPLESAMLPHP_UPSTREAM', getenv('HOME') ?: '/home/one' . '/repositories/simplesamlphp');
+define('SIMPLESAMLPHP_CONFIG_DIR', getenv('HOME') ?: '/home/one' . '/private/saml-configuration');
