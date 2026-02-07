37659e375e94e46066284c4ab616dc80cbc5f2fe
Ben Minter <benminter97@gmail.com>
2026-02-07 14:19:20 +0000

    enable htaccess

--- public/_bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-// _bootstrap.php
-define('SIMPLESAMLPHP_UPSTREAM', getenv('HOME') ?: '/home/one' . '/repositories/simplesamlphp');
-define('SIMPLESAMLPHP_CONFIG_DIR', getenv('HOME') ?: '/home/one' . '/private/saml-configuration');
